c4ee6ef1c8ad74d51efb5c2fd60dfc97862ecb08
dan derks <derks.dan@gmail.com>
2023-01-24 09:49:07 -0500

    adjust build.sh for macOS
    differentiates the `sed` arguments to replace `DATE` for macOS vs. Linux

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+os_ver=${1-$(uname -s)}
 list=$(ls -r ./*.md)
 for file in $list ; do
   date=$(date -r ${file} +%D)
@@ -10,7 +11,13 @@ for file in $list ; do
   cat head.htm_ > ${target}
   cmark --unsafe ${file}.md >> ${target}
   cat foot.htm_ >> ${target}
-  sed -i 's|DATE|'$date'|g' ${target}
+  if [[ "$os_ver" == "Darwin" ]]; then
+	  echo "macOS detected"
+	  sed -i '' -e 's#DATE#'$date'#g' ${target}
+  else
+	  echo "not macOS"
+	  sed -i 's|DATE|'$date'|g' ${target}
+  fi
 done
 
 sed -i -e '/GRID/r grid.htm_' -e '/NORNS/r norns.htm_' -e 'x;$G' index.html
